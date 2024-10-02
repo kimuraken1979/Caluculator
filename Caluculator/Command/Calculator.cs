01b532675f75922ee607f59464edb6a75de2b7c2
kimura <portovenere1997@yahoo.co.jp>
2024-10-02 15:10:01 +0900

    追加

---
+++ Caluculator/Command/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Command
+{
+    public class Calculator
+    {
+        public double ExecuteCommand(ICommand command)
+        {
+            return command.Execute();
+        }
+    }
+}
---
+++ Caluculator/TextButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class TextButton : Button
+    {
+        //public override string Text { get; set; }
+    }
+}
