7ce6061ce9c59d84e20958e00063b9a658335cd5
kimura <portovenere1997@yahoo.co.jp>
2024-10-02 14:32:51 +0900

    状態処理追加

--- Caluculator/CalculatorForm.Designer.cs
+++ Caluculator/CalculatorForm.Designer.cs
@@ -30,31 +30,311 @@ namespace Caluculator
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new TextButton();
+
+            inputTextBoxes = new TextBox[2];
+            no7TextButton = new TextButton();
+            outputTextBox = new TextBox();
+            textButton1 = new TextButton();
+            textButton2 = new TextButton();
+            textButton4 = new TextButton();
+            textButton5 = new TextButton();
+            textButton6 = new TextButton();
+            no8TextButton = new TextButton();
+            no9TextButton = new TextButton();
+            no6TextButton = new TextButton();
+            no5TextButton = new TextButton();
+            no4TextButton = new TextButton();
+            dotTextButton = new TextButton();
+            no0TextButton = new TextButton();
+            pmTextButton = new TextButton();
+            no3TextButton = new TextButton();
+            no2TextButton = new TextButton();
+            no1TextButton = new TextButton();
+            clearTextButton = new TextButton();
+            allClearTextButton = new TextButton();
             SuspendLayout();
             // 
-            // button1
+            // inputTextBoxes[0]
             // 
-            button1.Location = new Point(80, 172);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 1;
-            button1.Text = "1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            inputTextBoxes[0].Font = new Font("Yu Gothic UI", 20F);
+            inputTextBoxes[0].Location = new Point(12, 12);
+            inputTextBoxes[0].Name = "inputTextBoxes[0]";
+            inputTextBoxes[0].ReadOnly = true;
+            inputTextBoxes[0].Size = new Size(362, 52);
+            inputTextBoxes[0].TabIndex = 2;
+            inputTextBoxes[0].TextAlign = HorizontalAlignment.Right;
             // 
-            // Calculator
+            // no7TextButton
+            // 
+            no7TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no7TextButton.Location = new Point(409, 12);
+            no7TextButton.Name = "no7TextButton";
+            no7TextButton.Size = new Size(64, 52);
+            no7TextButton.TabIndex = 21;
+            no7TextButton.Text = "7";
+            no7TextButton.UseVisualStyleBackColor = true;
+            // 
+            // inputTextBoxes[1]
+            // 
+            inputTextBoxes[1].Font = new Font("Yu Gothic UI", 20F);
+            inputTextBoxes[1].Location = new Point(12, 161);
+            inputTextBoxes[1].Name = "inputTextBoxes[1]";
+            inputTextBoxes[1].ReadOnly = true;
+            inputTextBoxes[1].Size = new Size(362, 52);
+            inputTextBoxes[1].TabIndex = 33;
+            inputTextBoxes[1].TextAlign = HorizontalAlignment.Right;
+            // 
+            // outputTextBox
+            // 
+            outputTextBox.Font = new Font("Yu Gothic UI", 20F);
+            outputTextBox.Location = new Point(12, 315);
+            outputTextBox.Name = "outputTextBox";
+            outputTextBox.ReadOnly = true;
+            outputTextBox.Size = new Size(362, 52);
+            outputTextBox.TabIndex = 34;
+            outputTextBox.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textButton1
+            // 
+            textButton1.Font = new Font("Yu Gothic UI", 20F);
+            textButton1.Location = new Point(31, 85);
+            textButton1.Name = "textButton1";
+            textButton1.Size = new Size(64, 52);
+            textButton1.TabIndex = 35;
+            textButton1.Text = "+";
+            textButton1.UseVisualStyleBackColor = true;
+            // 
+            // textButton2
+            // 
+            textButton2.Font = new Font("Yu Gothic UI", 20F);
+            textButton2.Location = new Point(117, 85);
+            textButton2.Name = "textButton2";
+            textButton2.Size = new Size(64, 52);
+            textButton2.TabIndex = 36;
+            textButton2.Text = "-";
+            textButton2.UseVisualStyleBackColor = true;
+            // 
+            // textButton4
+            // 
+            textButton4.Font = new Font("Yu Gothic UI", 20F);
+            textButton4.Location = new Point(291, 85);
+            textButton4.Name = "textButton4";
+            textButton4.Size = new Size(64, 52);
+            textButton4.TabIndex = 38;
+            textButton4.Text = "÷";
+            textButton4.UseVisualStyleBackColor = true;
+            // 
+            // textButton5
+            // 
+            textButton5.Font = new Font("Yu Gothic UI", 20F);
+            textButton5.Location = new Point(205, 85);
+            textButton5.Name = "textButton5";
+            textButton5.Size = new Size(64, 52);
+            textButton5.TabIndex = 37;
+            textButton5.Text = "×";
+            textButton5.UseVisualStyleBackColor = true;
+            // 
+            // textButton6
+            // 
+            textButton6.Font = new Font("Yu Gothic UI", 20F);
+            textButton6.Location = new Point(158, 243);
+            textButton6.Name = "textButton6";
+            textButton6.Size = new Size(64, 52);
+            textButton6.TabIndex = 39;
+            textButton6.Text = "=";
+            textButton6.UseVisualStyleBackColor = true;
+            // 
+            // no8TextButton
+            // 
+            no8TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no8TextButton.Location = new Point(492, 12);
+            no8TextButton.Name = "no8TextButton";
+            no8TextButton.Size = new Size(64, 52);
+            no8TextButton.TabIndex = 40;
+            no8TextButton.Text = "8";
+            no8TextButton.UseVisualStyleBackColor = true;
+            // 
+            // no9TextButton
+            // 
+            no9TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no9TextButton.Location = new Point(575, 12);
+            no9TextButton.Name = "no9TextButton";
+            no9TextButton.Size = new Size(64, 52);
+            no9TextButton.TabIndex = 41;
+            no9TextButton.Text = "9";
+            no9TextButton.UseVisualStyleBackColor = true;
+            // 
+            // no6TextButton
+            // 
+            no6TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no6TextButton.Location = new Point(575, 85);
+            no6TextButton.Name = "no6TextButton";
+            no6TextButton.Size = new Size(64, 52);
+            no6TextButton.TabIndex = 44;
+            no6TextButton.Text = "6";
+            no6TextButton.UseVisualStyleBackColor = true;
+            // 
+            // no5TextButton
+            // 
+            no5TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no5TextButton.Location = new Point(492, 85);
+            no5TextButton.Name = "no5TextButton";
+            no5TextButton.Size = new Size(64, 52);
+            no5TextButton.TabIndex = 43;
+            no5TextButton.Text = "5";
+            no5TextButton.UseVisualStyleBackColor = true;
+            // 
+            // no4TextButton
+            // 
+            no4TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no4TextButton.Location = new Point(409, 85);
+            no4TextButton.Name = "no4TextButton";
+            no4TextButton.Size = new Size(64, 52);
+            no4TextButton.TabIndex = 42;
+            no4TextButton.Text = "4";
+            no4TextButton.UseVisualStyleBackColor = true;
+            // 
+            // dotTextButton
+            // 
+            dotTextButton.Font = new Font("Yu Gothic UI", 20F);
+            dotTextButton.Location = new Point(575, 233);
+            dotTextButton.Name = "dotTextButton";
+            dotTextButton.Size = new Size(64, 52);
+            dotTextButton.TabIndex = 50;
+            dotTextButton.Text = ".";
+            dotTextButton.UseVisualStyleBackColor = true;
+            // 
+            // no0TextButton
+            // 
+            no0TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no0TextButton.Location = new Point(492, 233);
+            no0TextButton.Name = "no0TextButton";
+            no0TextButton.Size = new Size(64, 52);
+            no0TextButton.TabIndex = 49;
+            no0TextButton.Text = "0";
+            no0TextButton.UseVisualStyleBackColor = true;
+            no0TextButton.Click += no0TextButton_Click;
+            // 
+            // pmTextButton
+            // 
+            pmTextButton.Font = new Font("Yu Gothic UI", 15F);
+            pmTextButton.Location = new Point(409, 233);
+            pmTextButton.Name = "pmTextButton";
+            pmTextButton.Size = new Size(64, 52);
+            pmTextButton.TabIndex = 48;
+            pmTextButton.Text = "+/-";
+            pmTextButton.UseVisualStyleBackColor = true;
+            // 
+            // no3TextButton
+            // 
+            no3TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no3TextButton.Location = new Point(575, 160);
+            no3TextButton.Name = "no3TextButton";
+            no3TextButton.Size = new Size(64, 52);
+            no3TextButton.TabIndex = 47;
+            no3TextButton.Text = "3";
+            no3TextButton.UseVisualStyleBackColor = true;
+            // 
+            // no2TextButton
+            // 
+            no2TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no2TextButton.Location = new Point(492, 160);
+            no2TextButton.Name = "no2TextButton";
+            no2TextButton.Size = new Size(64, 52);
+            no2TextButton.TabIndex = 46;
+            no2TextButton.Text = "2";
+            no2TextButton.UseVisualStyleBackColor = true;
+            // 
+            // no1TextButton
+            // 
+            no1TextButton.Font = new Font("Yu Gothic UI", 20F);
+            no1TextButton.Location = new Point(409, 160);
+            no1TextButton.Name = "no1TextButton";
+            no1TextButton.Size = new Size(64, 52);
+            no1TextButton.TabIndex = 45;
+            no1TextButton.Text = "1";
+            no1TextButton.UseVisualStyleBackColor = true;
+            no1TextButton.Click += no1TextButton_Click;
+            // 
+            // clearTextButton
+            // 
+            clearTextButton.Font = new Font("Yu Gothic UI", 20F);
+            clearTextButton.Location = new Point(409, 314);
+            clearTextButton.Name = "clearTextButton";
+            clearTextButton.Size = new Size(118, 52);
+            clearTextButton.TabIndex = 51;
+            clearTextButton.Text = "クリア";
+            clearTextButton.UseVisualStyleBackColor = true;
+            // 
+            // allClearTextButton
+            // 
+            allClearTextButton.Font = new Font("Yu Gothic UI", 15F);
+            allClearTextButton.Location = new Point(533, 315);
+            allClearTextButton.Name = "allClearTextButton";
+            allClearTextButton.Size = new Size(118, 52);
+            allClearTextButton.TabIndex = 52;
+            allClearTextButton.Text = "全クリア";
+            allClearTextButton.UseVisualStyleBackColor = true;
+            // 
+            // CalculatorForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Name = "Calculator";
-            Text = "Form1";
+            ClientSize = new Size(655, 384);
+            Controls.Add(allClearTextButton);
+            Controls.Add(clearTextButton);
+            Controls.Add(dotTextButton);
+            Controls.Add(no0TextButton);
+            Controls.Add(pmTextButton);
+            Controls.Add(no3TextButton);
+            Controls.Add(no2TextButton);
+            Controls.Add(no1TextButton);
+            Controls.Add(no6TextButton);
+            Controls.Add(no5TextButton);
+            Controls.Add(no4TextButton);
+            Controls.Add(no9TextButton);
+            Controls.Add(no8TextButton);
+            Controls.Add(textButton6);
+            Controls.Add(textButton4);
+            Controls.Add(textButton5);
+            Controls.Add(textButton2);
+            Controls.Add(textButton1);
+            Controls.Add(outputTextBox);
+            Controls.Add(inputTextBoxes[1]);
+            Controls.Add(no7TextButton);
+            foreach (var tb in inputTextBoxes)
+            {
+                Controls.Add(tb);
+            }
+            
+            Name = "CalculatorForm";
+            Text = "Calculator";
+            Load += CalculatorForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private TextButton button1;
+        private TextBox[] inputTextBoxes;
+        private TextButton no7TextButton;
+           private TextBox outputTextBox;
+        private TextButton textButton1;
+        private TextButton textButton2;
+        private TextButton textButton4;
+        private TextButton textButton5;
+        private TextButton textButton6;
+        private TextButton no8TextButton;
+        private TextButton no9TextButton;
+        private TextButton no6TextButton;
+        private TextButton no5TextButton;
+        private TextButton no4TextButton;
+        private TextButton dotTextButton;
+        private TextButton no0TextButton;
+        private TextButton pmTextButton;
+        private TextButton no3TextButton;
+        private TextButton no2TextButton;
+        private TextButton no1TextButton;
+        private TextButton clearTextButton;
+        private TextButton allClearTextButton;
     }
 }
--- Caluculator/CalculatorForm.cs
+++ Caluculator/CalculatorForm.cs
@@ -2,14 +2,55 @@ namespace Caluculator
 {
     public partial class CalculatorForm : Form
     {
+        private int indexInputTextBoxes = 0;
+
         public CalculatorForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
+        private void CalculatorForm_Load(object sender, EventArgs e)
+        {
+            Set1stState();
+        }
+
+        private void Set1stState()
+        {
+            SetInitState();
+
+            indexInputTextBoxes = 0;
+            SetActiveState();
+        }
+
+        private void SetInitState()
+        {
+            foreach (var tb in inputTextBoxes)
+            {
+                tb.Text = string.Empty;
+                tb.BackColor = SystemColors.Control;
+            }
+
+            outputTextBox.Text = string.Empty;
+            outputTextBox.BackColor = SystemColors.Control;
+        }
+
+        private void SetActiveState()
+        {
+            inputTextBoxes[indexInputTextBoxes].BackColor = Color.Wheat;
+        }
+
+        private void no0TextButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputTextBoxes[indexInputTextBoxes].Text) == false)
+            {
+                inputTextBoxes[indexInputTextBoxes].Text += "0";
+            }
+        }
+
+        private void no1TextButton_Click(object sender, EventArgs e)
+        {
+            inputTextBoxes[indexInputTextBoxes].Text += "1";
         }
     }
 }
